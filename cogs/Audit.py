5ec84c4238b562d0d4685b834a8ad5db8afd4767
Andr3as07 <andr3as06@gmail.com>
2020-08-25 14:56:59 +0200

    Started implementing Audit cog

---
+++ cogs/Audit.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+
+class Audit(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    async def print_audit(self, author, type, message):
+        usr = self.client.get_cog('Main').load_user(author.guild.id, author.id)
+        # TODO: Passed user class
+
+        # TODO: Check for user not found
+
+        # Check if the server audit log is enabled
+        if usr.server.audit_channel is None:
+            return False
+
+        # Check if the audit log channel exists
+        ch = self.client.get_channel(usr.server.audit_channel)
+        if ch is None:
+            return False
+
+        # TODO: Check if audits of this type are enabled
+
+        # Send audit log
+        await ch.send("<@!%s>: %s" % (usr.ID, message))
+
+        return True
+
+def setup(client):
+    client.add_cog(Audit(client))
